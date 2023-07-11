29d4db961e6d67f9181441156ce9d6dfdbebc003
vachalla <vachalla@INLEN8520042160.corp.capgemini.com>
2023-07-11 10:35:37 +0530

    commitss

---
+++ src/main/java/com/example/in/Springsecurityapp5Application.java
@@ -0,0 +1,13 @@
+package com.example.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsecurityapp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurityapp5Application.class, args);
+	}
+
+}
